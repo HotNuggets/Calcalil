4f35aa6ee02c0589ea86f502f1f7a264b028e55a
HotNuggets <mishamandark@gmail.com>
2026-03-24 12:50:30 +0200

    repair 2

--- client/Calcalil/src/view/pages/LoginPage/LoginPage.tsx
+++ client/Calcalil/src/view/pages/LoginPage/LoginPage.tsx
@@ -3,7 +3,7 @@ import { supabase } from '../../../lib/supabase'
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../../../contexts/AuthContext'
 import styles from './LoginPage.module.scss'
-import PageHeader from '../../components/HeaderComponent/PageHeader/PageHeader'
+
 
 const LoginPage = () => {
   const { user, loading } = useAuth()
