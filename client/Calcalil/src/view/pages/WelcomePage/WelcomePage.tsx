fc18144cf11b1f9afbe246fd3e0d062a40790096
HotNuggets <mishamandark@gmail.com>
2026-04-28 08:46:15 +0300

    update

--- client/Calcalil/src/view/pages/WelcomePage/WelcomePage.tsx
+++ client/Calcalil/src/view/pages/WelcomePage/WelcomePage.tsx
@@ -1,40 +1,39 @@
-//import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./WelcomePage.module.scss";
 import PageHeader from "../../components/HeaderComponent/PageHeader/PageHeader";
 import { Analytics } from "@vercel/analytics/react"
 import Footer from "../../components/Footer/Footer";
 
-
-
 const WelcomePage = () => {
   return (
     <div className={styles.welcome}>
-      <div className={styles.userMenuContainer}>
-        <PageHeader showBackButton={false} />
+      <PageHeader showBackButton={false} />
+      <Analytics /> 
+      
+      <div className={styles.content}>
+        <h1 className={styles.title}>ברוך הבא!</h1>
+        <p className={styles.subtitle}>בחר מחשבון כדי להתחיל:</p>
         
+        <div className={styles.buttons}>
+          <Link to="/loan">
+            <button>מחשבון הלוואות</button>
+          </Link>
+          <Link to="/savings">
+            <button>מחשבון חיסכון</button>
+          </Link> 
+          <Link to="/expenses">
+            <button>מעקב הוצאות</button>
+          </Link> 
+          <Link to="/SNP">
+            <button>מדד סנ"פ בישראל</button>
+          </Link> 
+          <Link to="/Salary">
+            <button>מחשבון צמיחת שכר</button>
+          </Link> 
+        </div>
       </div>
-        <Analytics /> 
-      <h1 className={styles.title}>ברוך הבא!</h1>
-      <p className={styles.subtitle}>בחר מחשבון כדי להתחיל:</p>
-      <div className={styles.buttons}>
-        <Link to="/loan">
-          <button>מחשבון הלוואות</button>
-        </Link>
-        <Link to="/savings">
-          <button>מחשבון חיסכון</button>
-        </Link> 
-         <Link to="/expenses">
-          <button>מעקב הוצאות</button>
-        </Link> 
-        <Link to="/SNP">
-          <button>מדד סנ"פ בישראל</button>
-        </Link> 
-        <Link to="/Salary">
-          <button>מחשבון צמיחת שכר</button>
-        </Link> 
-      </div>
-       <Footer />
+      
+      <Footer />
     </div>
   );
 };
